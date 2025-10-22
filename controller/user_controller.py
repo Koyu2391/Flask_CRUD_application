fc6f0717dcdadac9bd7923c3620676df29804a2d
Koyu2391 <muhammad.fahad369@gmail.com>
2025-10-22 13:51:27 +0530

    fix(read, update): CORS issue solved

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -5,28 +5,27 @@ from app import app
 
 obj = user_model()
 
-# for Create -> fahad
+
 @app.route("/sign_up", methods=["POST"])
 def create():
     return obj.create_c(request.get_json())
 
-# for read -> nameera 
+
 @app.route("/user/getall", methods=["GET"])
 def user_getall_controller():
-    data = request.json
-    return obj.read_r(data)
+    return obj.getall()
 
-#for update -> nameera 
+
 @app.route("/user/update/<id>", methods=["PUT"])
-def user_update_controller():
-    return obj.user_update_model()
+def user_update_controller(id):
+    return obj.update(id, data)
 
-#for Delete -> Fahad   
+
 @app.route("/delete_user/<id>", methods=["DELETE"])
 def delete(id):
     return obj.delete_d(id)
 
-# Additional Feature For user login 
+
 @app.route('/log_in', methods = ["POST"])
 def user_login():
     return obj.login_l(request.get_json())
--- model/user_read.py
+++ model/user_read.py
@@ -1,16 +1,16 @@
 from flask import jsonify
 import sqlite3
 
-def read_user(self):
-        con = sqlite3.connect(self.db_name)
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        cur.execute("SELECT * FROM user")
-        rows = cur.fetchall()
-        result = [dict(row) for row in rows]
-        con.close()
+def read_user(db_name):
+    con = sqlite3.connect(db_name)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM user")
+    rows = cur.fetchall()
+    result = [dict(row) for row in rows]
+    con.close()
 
-        if result:
-            return jsonify({"database": result})
-        else:
-            return jsonify({"message": "No data found"}), 404
+    if result:
+        return jsonify({"database": result})
+    else:
+        return jsonify({"message": "No data found"}), 404
