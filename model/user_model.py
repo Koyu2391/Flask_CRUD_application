1dbec0547e7475d75a13551e3aa69a0382075395
Nameera Ahmad <ahmadnameera77@gmail.com>
2025-10-22 12:38:40 +0530

    feat:add model

--- model/user_model.py
+++ model/user_model.py
@@ -3,68 +3,36 @@ from flask import jsonify, make_response
 from .user_create import create_user
 from .user_login import login_user
 from .user_delete import delete_user
-
+from .user_read import read_user
+from .user_update import update_user
 
 class user_model:
     def __init__(self):
         try:
-            self.con = sqlite3.connect("geeky_macet.db", check_same_thread=False)
+            self.db_name = "geeky_macet.db"
+            self.con = sqlite3.connect(self.db_name, check_same_thread=False)
             self.con.row_factory = sqlite3.Row
             self.cur = self.con.cursor()
             print("Sql Connection Estd")
         except Exception as e:
-            print(f"Some Error Occured {e}")
-    # Fahad's Side of the work Includes -> (Create and Delete)
+            print(f"Some Error occured {e}")
+
+# Fahad's Side of the work Includes -> (Create and Delete)
 
     def create_c(self, data):
         return create_user(self.cur, self.con, data)
 
     def delete_d(self, id):
         return delete_user(self.cur, self.con, id)
-
-    def login_l(self, data): # Extra Login Feature 
+    
+# Extra Login Feature
+    def login_l(self, data):  
         return login_user(self.cur, self.con, data)
 
-    # End of the specified Work
-
-    # READ
-    def user_getall_model(self):
-        con = sqlite3.connect(self.db_name)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM user")
-        result = cur.fetchall()
-        con.close()
-
-        if result:
-            return jsonify({"payload": result})
-        else:
-            return jsonify({"message": "No data found"}), 404
-
-    # UPDATE
-    def user_update_model(self, data):
-        con = sqlite3.connect(self.db_name)
-        cur = con.cursor()
-        try:
-            cur.execute(
-                "UPDATE user SET name=?, email=?, password=?, roll_batch=?, branch=? WHERE id=?",
-                (
-                    data["name"],
-                    data["email"],
-                    data["password"],
-                    data["roll_batch"],
-                    data.get("branch"),
-                    data["id"],
-                ),
-            )
-            con.commit()
-
-            if cur.rowcount > 0:
-                return make_response({"message": "UPDATED_SUCCESSFULLY"}, 201)
-            else:
-                return make_response({"message": "NOTHING_TO_UPDATE"}, 204)
-        except Exception as e:
-            import traceback
-
-            print("Update error:", e)
-            traceback.print_exc()
-            return make_response({"message": "UPDATE_FAILED", "error": str(e)}, 500)
+    
+# Nameera's side of the work includes READ and UPDATE
+    def read_r(self):
+        return read_user(self.cur, self.con)
+    
+    def update_u(self, data):
+        return update_user(self.con, data)
